d627758344cbd4c6266e51a3cf4c8c287b3a5bbb
Theis Egeberg <theis.egeberg@gmail.com>
2021-06-21 11:32:56 +0200

    Added a more friendly zip

--- Sources/TTEResult/Result+Zip.swift
+++ Sources/TTEResult/Result+Zip.swift
@@ -13,3 +13,12 @@ public func zip<A, B, Failure>(_ a: Result<A, Failure>, _ b: Result<B, Failure>)
     }
   }
 }
+
+/// Zips A? and B? together into a tuple of (A,B)?
+/// - Parameters:
+///   - a: Optional A
+///   - b: Optional B
+/// - Returns: Tuple of A and B
+public func zip<A, B>(_ a: A?, _ b: B?) -> (A, B)? {
+  return a.flatMap { a in b.map { b in (a, b) } }
+}
